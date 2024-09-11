356310a1f37b7f04df06c30608874cc25d7a3c01
Pirat <intake@list.ru>
2024-09-11 22:03:03 +0300

    config changed

--- GitBashPractice/obj/Debug/net8.0/GitBashPractice.AssemblyInfo.cs
+++ GitBashPractice/obj/Debug/net8.0/GitBashPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitBashPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0cfe646c6b6977856cb312f830f8e829eedd18")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitBashPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitBashPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
