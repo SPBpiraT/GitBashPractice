a3d546be315cdda80c7e0627fa303386e5405f44
Pirat <intake@list.ru>
2024-09-16 01:19:36 +0300

    merge test conflicts

--- GitBashPractice/Program.cs
+++ GitBashPractice/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Hello, World!");
 for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("yo yo yo");
+    Console.WriteLine("yo yo sssyo");
     Console.WriteLine("yo syo yos");
 
 
