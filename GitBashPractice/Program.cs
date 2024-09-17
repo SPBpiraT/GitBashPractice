bce980b5a039aee9b5ebaf505b7488a98066409b
Pirat <intake@list.ru>
2024-09-17 03:11:23 +0300

    origin changed

--- GitBashPractice/Program.cs
+++ GitBashPractice/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Hello, World!");
 for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("yo yo sssyo");
+    Console.WriteLine("yo yo sso");
     Console.WriteLine("yo syo yos");
 
 
