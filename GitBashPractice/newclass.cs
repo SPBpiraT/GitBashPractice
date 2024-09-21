6f2cae91acd467d95fdc4cab7ff8a784b75a86d8
SPBpiraT <intake@list.ru>
2024-09-21 23:31:09 +0300

    ne

--- GitBashPractice/newclass.cs
+++ GitBashPractice/newclass.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace GitBashPractice
 {
     internal class newclass
     {
+        public void CW()
+        {
+            Console.WriteLine("ne");
+        }
     }
 }
