3f7cd32808e16ee28aadcbc6f09594f1b72f8411
SPBpiraT <intake@list.ru>
2024-09-21 23:01:56 +0300

    added sss

--- GitBashPractice/Program.cs
+++ GitBashPractice/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Hello, World!");
 for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("suck mu cube");
+    Console.WriteLine("suck mu cubesss");
     Console.WriteLine("yo syo yos");
 
 
