27896b104190a47c5cc5ac45315e9de6e56fd1ce
SPBpiraT <intake@list.ru>
2024-09-23 00:32:31 +0300

    ss

--- GitBashPractice/Class1.cs
+++ GitBashPractice/Class1.cs
@@ -12,7 +12,7 @@ namespace GitBashPractice
         {
             Console.WriteLine("ne");
             Console.WriteLine("ne");
-            Console.WriteLine("ne");
+            Console.WriteLine("nsse");
         }
     }
 }
